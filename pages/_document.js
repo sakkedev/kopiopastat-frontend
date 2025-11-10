dc53865abb7c1808200288bba55d62fc9fcecc83
sakkedev <sakkedev@users.noreply.github.com>
2025-11-10 20:14:48 +0200

    Favicon change

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/kopiothumbnail.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <script
